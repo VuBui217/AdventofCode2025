32abccee696b8c94952368df3760e7dc597c13ed
VuBui217 <vubui217@gmail.com>
2025-12-16 16:47:11 -0500

    Refactor part 1 and add part 2 solution

--- day3/lobby.py
+++ day3/lobby.py
@@ -1,16 +1,47 @@
-def max_joltage(battery):
-    res = 0
-    for i in range(len(battery) - 1):
-        for j in range(i + 1, len(battery)):
-            res = max(res, int(battery[i] + battery[j]))
-    return res
+# Part 1
+def max_joltage_part1(battery):
+
+    # Helper function
+    def find_largest(curr_battery):
+        return max(curr_battery), curr_battery.index(max(curr_battery))
+
+    
+    largest, largest_idx = find_largest(battery)
+    if largest_idx != len(battery) - 1:
+        
+        next_largest, next_largest_idx = find_largest(battery[largest_idx + 1:])
+        return int(largest + next_largest)
+    else:
+        next_largest, next_largest_idx = find_largest(battery[:largest_idx])
+        return int(next_largest + largest)
+
+# Part 2
+def max_joltage_part2(battery):
+    answer = ""
+    for i in range(12, 0, -1):
+        largest, largest_idx = next_max_digit(battery, i)
+        answer += largest
+        battery = battery[largest_idx + 1:]
+    return int(answer)
+
+def next_max_digit(battery, remaining_digit_count):
+    if len(battery) == remaining_digit_count:
+        return battery[0], 0
+    largest = max(battery)
+    largest_index = battery.index(largest)
+    if largest_index <= len(battery) - remaining_digit_count:
+        return largest, largest_index
+    return next_max_digit(battery[:largest_index], remaining_digit_count - (len(battery) - largest_index))
+    
+
 def find_joltage(file):
     file_object = open(file, "r")
     total = 0
     for line in file_object:
         battery = line.strip()
-        total += max_joltage(battery)
-        print(battery)
+        # print(max_voltage(battery))
+        total += max_joltage_part2(battery)
+        
     file_object.close()
     return total
 

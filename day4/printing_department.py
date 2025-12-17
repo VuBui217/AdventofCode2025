4f4cbe258ac45559ddfb457a74c546a79ced075c
VuBui217 <vubui217@gmail.com>
2025-12-16 21:22:26 -0500

    Refactor part 1 and add part 2

--- day4/printing_department.py
+++ day4/printing_department.py
@@ -5,27 +5,51 @@ def find_rolls_of_paper(file):
     grid = []
     for line in file_object:
         line = line.strip()
-        grid.append(line)
+        grid.append(list(line))
     rows = len(grid)
     cols = len(grid[0])
     # Part 1
-    def can_access(r, c):
-        directions = [(1, 0), (-1, 0), (0, 1), (0, -1), (-1, -1), (-1, 1), (1, -1), (1, 1)]
-        count = 0
-        for dr, dc in directions:
-            new_r = r + dr
-            new_c = c + dc
-            if 0 <= new_r < rows and 0 <= new_c < cols and grid[new_r][new_c] == "@":
-                count += 1
-                if count > 3:
-                    return False
-        return True
+    directions = []
+    for i in [-1, 0, 1]:
+        for j in [-1, 0, 1]:
+            if i != 0 or j != 0:
+                directions.append((i, j))
+    print(directions)
+    def is_roll(current_position, direction):
+        if 0 <= (current_position[0] + direction[0]) < rows and 0 <= (current_position[1] + direction[1]) < cols and grid[current_position[0] + direction[0]][current_position[1] + direction[1]] == "@":
+            return True
+        else:
+            return False
     
-    for r in range(rows):
-        for c in range(cols):
-            if grid[r][c] == "@" and can_access(r, c):
-                res += 1
-            
+    def roll_count(current_position):
+        return sum([is_roll(current_position, direction) for direction in directions])
+    # Part 1
+    # for r in range(rows):
+    #     for c in range(cols):
+
+    #         if grid[r][c] == "@" and roll_count((r,c)) < 4:
+    #             res += 1
+
+    # Part 2
+    while True:
+        to_remove = []
+        curr = 0
+        for r in range(rows):
+            for c in range(cols):
+                if grid[r][c] == "@" and roll_count((r, c)) < 4:
+                    # to_remove.append((r, c)) 
+                    curr += 1
+                    grid[r][c] = "."
+
+        if curr == 0:
+            break
+        else:
+            res += curr
+        # if not to_remove:
+        #     break
+        # for r, c in to_remove:
+        #     grid[r][c] = "."
+        # res += len(to_remove)
     file_object.close()
     # print(grid)
     return res
